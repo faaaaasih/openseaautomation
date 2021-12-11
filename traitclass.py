f92bb67637401fe634f11958ba09725dc82d7742
faaaaasih <fasihq38@gmail.com>
2021-12-11 19:45:15 +0000

    new files

---
+++ traitclass.py
@@ -0,0 +1,48 @@
+face_files = {
+    "White": "face1",
+    "Black": "face2"
+}
+
+ears_files = {
+    "ears1": "ears1",
+    "ears2": "ears2",
+    "ears3": "ears3",
+    "ears4": "ears4"
+}
+
+eyes_files = {
+    "regular": "eyes1",
+    "small": "eyes2",
+    "rayban": "eyes3",
+    "hipster": "eyes4",
+    "focused": "eyes5"     
+}
+
+hair_files = {
+    "hair1": "hair1",
+    "hair2": "hair2",
+    "hair3": "hair3",
+    "hair4": "hair4",
+    "hair5": "hair5",
+    "hair6": "hair6",
+    "hair7": "hair7",
+    "hair8": "hair8",
+    "hair9": "hair9",
+    "hair10": "hair10",
+    "hair11": "hair11",
+    "hair12": "hair12"
+}
+
+mouth_files = {
+    "m1": "m1",
+    "m2": "m2",
+    "m3": "m3",
+    "m4": "m4",
+    "m5": "m5",
+    "m6": "m6"
+}
+
+nose_files = {
+    "n1": "n1",
+    "n2": "n2"   
+}
