ecb15a93bae0b32bd52f917f214b13f9c451d365
faaaaasih <fasihq38@gmail.com>
2021-12-11 19:40:02 +0000

    add new files

---
+++ pythonpackage.py
@@ -0,0 +1,5 @@
+from PIL import Image
+from IPython.display import display
+import random
+import json
+import os
---
+++ rarity.py
@@ -0,0 +1,22 @@
+face = ["White", "Black"] 
+face_weights = [60, 40]
+
+ears = ["ears1", "ears2", "ears3", "ears4"] 
+ears_weights = [25, 30 , 44, 1]
+
+eyes = ["regular", "small", "rayban", "hipster", "focused"] 
+eyes_weights = [70, 10, 5, 1, 14]
+
+hair = ['hair1', 'hair10', 'hair11', 'hair12', 'hair2', 'hair3', 'hair4',
+ 'hair5',
+ 'hair6',
+ 'hair7',
+ 'hair8',
+ 'hair9']
+hair_weights = [10 , 10 , 10 , 10 ,10, 10, 10 ,10 ,10, 7 , 1 , 2]
+
+mouth = ['m1', 'm2', 'm3', 'm4', 'm5', 'm6']
+mouth_weights = [10, 10,50, 10,15, 5]
+
+nose = ['n1', 'n2']
+nose_weights = [90, 10]
